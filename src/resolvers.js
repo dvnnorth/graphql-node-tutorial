53a26d06a2e195fe8c7edd4ce0b6a894d82c46de
dvnnorth <13654540+dvnnorth@users.noreply.github.com>
2019-05-12 21:25:55 -0500

    Added the bonus CRUD functionality (update and delete mutations)

--- src/resolvers.js
+++ src/resolvers.js
@@ -15,7 +15,8 @@ let links = [
 module.exports = {
   Query: {
     info: () => "This is the API of a Hackernews Clone",
-    feed: () => links
+    feed: () => links,
+    link: (_, args) => links.filter(link => link.id === args.id)[0]
   },
   Mutation: {
     post: (parent, args) => {
@@ -26,11 +27,51 @@ module.exports = {
       };
       links.push(post);
       return post;
+    },
+    updateLink: (parent, args) => {
+      // Signature updateLink(id: ID!, url: String, description: String): Link
+      if (args.id === null || typeof args.id === undefined) {
+        return null;
+      }
+
+      // Use a filter to grab the link to be updated
+      const oldLink = links.filter(link => link.id === args.id)[0] || {};
+
+      // Build updated link with args and oldLink information
+      const updatedLink = {
+        id: args.id || oldLink.id,
+        url: args.url || oldLink.url,
+        description: args.description || oldLink.description
+      };
+
+      // Create new links by mapping links and replacing link to be updated
+      // by matching the id
+      links = links.map(link => {
+        if (link.id === args.id) {
+          return { ...updatedLink };
+        } else {
+          return link;
+        }
+      });
+
+      // Send back the updated link or null if updated link is missing info
+      // updatedLink will only have a falsy property if an incorrect ID was
+      // provided
+      return updatedLink.id && updatedLink.url && updatedLink.description
+        ? { ...updatedLink }
+        : null;
+    },
+    // Signature deleteLink(id: ID!): Link
+    deleteLink: (parent, args) => {
+      // Remove link from links by id or null if not found (bad ID)
+      const deletedLink = links.filter(link => link.id === args.id)[0] || null;
+      links = links.filter(link => link.id !== args.id);
+      return deletedLink;
     }
-  },
-  Link: {
-    id: parent => parent.id,
-    description: parent => parent.description,
-    url: parent => parent.url
   }
+  // Link resolvers are not needed, assumed by Prisma because they're simply the
+  // properties, id, description, and url
+  // From the tut: First off, note that you’re entirely removing the Link
+  // resolvers (as explained before). They are not needed because the GraphQL
+  // server infers what they look like.
 };
